e815bbd6ebf7b46e9f70f5fc85b2e11eb01382b3
UneuraLolli <120707623+uonlraSnaey@users.noreply.github.com>
2024-07-02 08:53:33 +0800

    Add files via upload

---
+++ nvim_lua/lua/plugin/dashboard.lua
@@ -0,0 +1,124 @@
+local status, db = pcall(require, "dashboard")
+if not status then
+  vim.notify("没有找到 dashboard")
+  return
+end
+
+db.setup({
+  theme = "doom",
+  config = {
+    -- week_header = {
+    --   enable = true,
+    -- },
+    footer = {
+      "",
+      "",
+      " https://github.com/uonlraSnaey",
+    },
+
+    center = {
+      {
+        icon = "                    󰈞  ",
+        icon_hl = "title",
+        desc = "Find file                           ",
+        desc_hl = "string",
+        action = "Telescope find_files",
+      },
+      -- {
+      --   icon = "  ",
+      --   desc = "Projects                            ",
+      --   action = "Telescope projects",
+      -- },
+      -- {
+      --   icon = "  ",
+      --   desc = "Recently files                      ",
+      --   action = "Telescope oldfiles",
+      -- },
+      {
+        icon = "                      ",
+        icon_hl = "title",
+        desc = "Edit keybindings                    ",
+        desc_hl = "string",
+        action = "edit D:/CppApp/neovim/nvim/lua/keybindings.lua",
+      },
+      -- {
+      --   icon = "  ",
+      --   desc = "Edit Projects                       ",
+      --   action = "edit ~/.local/share/nvim/project_nvim/project_history",
+      -- },
+      {
+        icon = "                      ",
+        icon_hl = "title",
+        desc = "Edit PowerShell                     ",
+        desc_hl = "string",
+        action = "edit D:/UsersData/Documents/PowerShell/Microsoft.PowerShell_profile.ps1",
+      },
+      -- {
+      --   icon = '            ',
+      --   desc = 'Change colorscheme                  ',
+      --   action = ':ColorScheme',
+      -- },
+      {
+        icon = "                      ",
+        icon_hl = "title",
+        desc = "Edit init.lua                       ",
+        desc_hl = "string",
+        action = "edit D:/CppApp/neovim/nvim/init.lua",
+      },
+      -- {
+      --   icon = "  ",
+      --   desc = "Find text                           ",
+      --   action = "Telescopecope live_grep",
+      -- },
+    },
+
+    header = {
+      [[]],
+      [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+      [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+      [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+      [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+      [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+      [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+      [[                                                   ]],
+      [[                [ version : 0.10.0 ]                ]], 
+      [[]],
+      [[]],
+    },
+  },
+})
+
+-- db.custom_header = {
+--     [[]],
+--     [[███╗   ██╗███████╗██╗  ██╗███████╗███╗   ██╗]],
+--     [[████╗  ██║██╔════╝██║  ██║██╔════╝████╗  ██║]],
+--     [[██╔██╗ ██║███████╗███████║█████╗  ██╔██╗ ██║]],
+--     [[██║╚██╗██║╚════██║██╔══██║██╔══╝  ██║╚██╗██║]],
+--     [[██║ ╚████║███████║██║  ██║███████╗██║ ╚████║]],
+--     [[╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝]],
+-- }
+--
+-- db.custom_header = {
+--     [[]],
+--     [[          ▀████▀▄▄              ▄█ ]],
+--     [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+--     [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
+--     [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
+--     [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
+--     [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+--     [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
+--     [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
+--     [[   █   █  █      ▄▄           ▄▀   ]],
+-- }
+--
+db.custom_header = {
+     [[]],
+     [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+     [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+     [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+     [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+     [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+     [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+     [[                                                   ]],
+     [[                [ version : 11.45.14 ]                ]],
+}
